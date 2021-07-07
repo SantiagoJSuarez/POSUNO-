b86ecc6271a39b2f17242cff202d669e66593a58
SantiagoJSuarez <58789506+SantiagoJSuarez@users.noreply.github.com>
2021-07-07 15:47:09 -0500

    Api Controller

---
+++ POSUNO/POSUNO.Api/Models/LoginRequest.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace POSUNO.Api.Models
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
