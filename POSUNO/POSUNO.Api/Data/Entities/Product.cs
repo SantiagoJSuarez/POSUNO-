f71ea354df8814e4c5a3751389adf86fe9934441
SantiagoJSuarez <58789506+SantiagoJSuarez@users.noreply.github.com>
2021-07-07 15:08:36 -0500

    Data Base Creation

---
+++ POSUNO/POSUNO.Api/Data/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Api.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required] //Volver el campo obligatorio
+        [MaxLength(50)] //Maximo de caracteres
+        public string Name { get; set; }
+
+
+        [Required] //Volver el campo obligatorio
+        [MaxLength(500)] //Maximo de caracteres
+        public string Description { get; set; }
+
+
+        public decimal Price { get; set; }
+
+        public float Stock { get; set; } //Cantidad de producto en inventario
+
+        public User User { get; set; } //Relacion de un 1 a varios (Un producto pertenece a un Usuario y un Usuario tiene muchos productos)
+    }
+}
---
+++ POSUNO/POSUNO.Api/Data/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Api.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required] //Volver el campo obligatorio
+        [EmailAddress] //Nos verifica si es un email valido 
+        public string Email { get; set; }
+
+
+        [Required] //Volver el campo obligatorio
+        [MaxLength(50)] //Maximo de caracteres
+        public string FirstName { get; set; }
+
+        [Required] //Volver el campo obligatorio
+        [MaxLength(50)] //Maximo de caracteres
+        public string LastName { get; set; }
+
+        [Required] //Volver el campo obligatorio
+        [MaxLength(20)] //Maximo de caracteres
+        [MinLength(6)] //Minimo de caracteres
+        public string Password { get; set; }
+
+        //Relacion de un 1 a varios (Un producto pertenece a un Usuario y un Usuario tiene muchos productos)
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
