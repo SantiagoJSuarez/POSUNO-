955f0f19675ce72e47297870736bd884d5981564
SantiagoJSuarez <58789506+SantiagoJSuarez@users.noreply.github.com>
2021-07-12 15:18:32 -0500

    Product/customer Lists

--- POSUNO/POSUNO/POSUNO.Shared/Models/Product.cs
+++ POSUNO/POSUNO/POSUNO.Shared/Models/Product.cs
@@ -15,7 +15,11 @@ namespace POSUNO.Models
 
         public decimal Price { get; set; }
 
+        public string PriceString => $"{Price:C2}";
+
         public float Stock { get; set; } //Cantidad de producto en inventario
+
+        public string StockString => $"{Stock:N2}";
 
         public bool IsActive { get; set; }
 
